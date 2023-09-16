86a2c037d9c5eb023a6c0ba28310e8c3aa431558
Neranjana Prasad <neranjanaprsd@gmail.com>
2023-09-16 17:45:17 +0530

    Initialize Dev Branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    container: {
-      padding: "1rem",
-      center: true,
-    },
     extend: {
-      width: {
-        avatar: "6rem",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      maxWidth: {
-        avatar: "420px",
-      },
-      fontFamily: {
-        jbmono: ['"JetBrains Mono"', "monospace"],
-      },
-    },
-    screens: {
-      "2xl": { max: "1535px" },
-      xl: { max: "1279px" },
-      lg: { max: "1023px" },
-      md: { max: "767px" },
-      xmd: { max: "830px" },
-      sm: { max: "639px" },
-      mobile: "640px",
     },
   },
   plugins: [],
-};
+}
