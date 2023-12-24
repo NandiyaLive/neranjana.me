7ce8b48aee63f62ee9976a92887361f7240b85a5
Neranjana Prasad <neranjanaprsd@gmail.com>
2023-12-24 09:21:02 +0530

    fix: uses page layout

--- src/app/uses/data.js
+++ src/app/uses/data.js
@@ -17,133 +17,382 @@ export const hardware = [
   },
 ];
 
-export const devSoft = [
+// export const devSoft = [
+//   {
+//     name: "Operating System",
+//     description: "Manjaro 23 - Vulcan (XFCE)",
+//     url: "https://manjaro.org",
+//   },
+//   {
+//     name: "IDE",
+//     description: "Visual Studio Code",
+//     url: "https://code.visualstudio.com",
+//   },
+//   {
+//     name: "VS Code Theme",
+//     description: "Andromeda",
+//     url: "https://marketplace.visualstudio.com/items?itemName=EliverLara.andromeda",
+//   },
+//   {
+//     name: "AI Pair Programmer",
+//     description: "GitHub Copilot",
+//     url: "https://copilot.github.com",
+//   },
+//   {
+//     name: "Online IDE",
+//     description: "CodeSandbox",
+//     url: "https://codesandbox.io",
+//   },
+//   {
+//     name: "Shell",
+//     description: "fish",
+//     url: "https://fishshell.com",
+//   },
+//   {
+//     name: "Prompt",
+//     description: "Starship",
+//     url: "https://starship.rs",
+//   },
+//   {
+//     name: "Version Control",
+//     description: "GitHub",
+//     url: "https://github.com",
+//   },
+//   {
+//     name: "Hosting",
+//     description: "Vercel",
+//     url: "https://vercel.com",
+//   },
+//   {
+//     name: "API Testing",
+//     description: "Insomnia & Postman",
+//     url: "https://insomnia.rest",
+//   },
+//   {
+//     name: "DNS",
+//     description: "Cloudflare",
+//     url: "https://cloudflare.com",
+//   },
+//   {
+//     name: "Prototyping",
+//     description: "Figma",
+//     url: "https://figma.com",
+//   },
+//   {
+//     name: "Illustrations",
+//     description: "unDraw",
+//     url: "https://undraw.co",
+//   },
+//   {
+//     name: "Icons",
+//     description: "Lucide / Heroicons / FontAwesome",
+//     url: "https://lucide.dev",
+//   },
+// ];
+
+// export const otherSoft = [
+//   {
+//     name: "Music",
+//     description: "Spotify & YouTube Music",
+//     url: "https://open.spotify.com/playlist/4oKjrCt8bfJCLQxL635Ftq",
+//   },
+//   {
+//     name: "Password Manager",
+//     description: "Bitwarden",
+//     url: "https://bitwarden.com",
+//   },
+//   {
+//     name: "2FA",
+//     description: "Twilio Authy",
+//     url: "https://authy.com",
+//   },
+//   {
+//     name: "Main Browser",
+//     description: "Mozilla Firefox",
+//     url: "https://mozilla.org/en-US/firefox",
+//   },
+//   {
+//     name: "Secondary Browser",
+//     description: "Brave",
+//     url: "https://brave.com",
+//   },
+//   {
+//     name: "Email Client",
+//     description: "MailSpring",
+//     url: "https://getmailspring.com",
+//   },
+//   {
+//     name: "Ad Blocker",
+//     description: "uBlock Origin",
+//     url: "https://ublockorigin.com",
+//   },
+//   {
+//     name: "Cloud Storage",
+//     description: "Telegram",
+//     url: "https://telegram.org",
+//   },
+//   {
+//     name: "Media Server",
+//     description: "Jellyfin",
+//     url: "https://jellyfin.org",
+//   },
+//   // {
+//   //   name: "Media Player",
+//   //   description: "VLC",
+//   //   url: "https://videolan.org/vlc/index.html",
+//   // },
+//   {
+//     name: "TV / Movies Tracker",
+//     description: "Trakt.tv",
+//     url: "https://trakt.tv",
+//   },
+//   {
+//     name: "Note Taking",
+//     description: "Notion",
+//     url: "https://notion.so",
+//   },
+// ];
+
+export const softwaresList = [
   {
     name: "Operating System",
-    description: "Manjaro Linux (XFCE)",
-    url: "https://manjaro.org",
+    softwares: [
+      {
+        name: "Manjaro 23 - Vulcan (XFCE)",
+        url: "https://manjaro.org",
+      },
+    ],
   },
   {
     name: "IDE",
-    description: "Visual Studio Code",
-    url: "https://code.visualstudio.com",
+    softwares: [
+      {
+        name: "Visual Studio Code",
+        url: "https://code.visualstudio.com",
+      },
+    ],
   },
   {
     name: "VS Code Theme",
-    description: "Andromeda",
-    url: "https://marketplace.visualstudio.com/items?itemName=EliverLara.andromeda",
+    softwares: [
+      {
+        name: "Andromeda",
+        url: "https://marketplace.visualstudio.com/items?itemName=EliverLara.andromeda",
+      },
+    ],
+  },
+  {
+    name: "AI Pair Programmer",
+    softwares: [
+      {
+        name: "GitHub Copilot",
+        url: "https://copilot.github.com",
+      },
+    ],
   },
   {
     name: "Online IDE",
-    description: "CodeSandbox",
-    url: "https://codesandbox.io",
+    softwares: [
+      {
+        name: "CodeSandbox",
+        url: "https://codesandbox.io",
+      },
+    ],
   },
   {
     name: "Shell",
-    description: "fish",
-    url: "https://fishshell.com",
+    softwares: [
+      {
+        name: "fish",
+        url: "https://fishshell.com",
+      },
+    ],
   },
   {
     name: "Prompt",
-    description: "Starship",
-    url: "https://starship.rs",
+    softwares: [
+      {
+        name: "Starship",
+        url: "https://starship.rs",
+      },
+    ],
   },
   {
     name: "Version Control",
-    description: "GitHub",
-    url: "https://github.com",
+    softwares: [
+      {
+        name: "GitHub",
+        url: "https://github.com",
+      },
+    ],
   },
   {
     name: "Hosting",
-    description: "Vercel",
-    url: "https://vercel.com",
+    softwares: [
+      {
+        name: "Vercel",
+        url: "https://vercel.com",
+      },
+    ],
   },
   {
     name: "API Testing",
-    description: "Insomnia (Poastman is good too)",
-    url: "https://insomnia.rest",
+    softwares: [
+      {
+        name: "Insomnia",
+        url: "https://insomnia.rest",
+      },
+      {
+        name: "Postman",
+        url: "https://www.postman.com",
+      },
+    ],
   },
   {
     name: "DNS",
-    description: "Cloudflare",
-    url: "https://cloudflare.com",
+    softwares: [
+      {
+        name: "Cloudflare",
+        url: "https://cloudflare.com",
+      },
+    ],
   },
   {
     name: "Prototyping",
-    description: "Figma",
-    url: "https://figma.com",
+    softwares: [
+      {
+        name: "Figma",
+        url: "https://figma.com",
+      },
+    ],
   },
   {
     name: "Illustrations",
-    description: "unDraw",
-    url: "https://undraw.co",
+    softwares: [
+      {
+        name: "unDraw",
+        url: "https://undraw.co",
+      },
+    ],
   },
   {
     name: "Icons",
-    description: "Lucide / Heroicons / FontAwesome",
-    url: "https://lucide.dev",
+    softwares: [
+      {
+        name: "Lucide",
+        url: "https://lucide.dev",
+      },
+      {
+        name: "Heroicons",
+        url: "https://heroicons.com",
+      },
+      {
+        name: "FontAwesome",
+        url: "https://fontawesome.com",
+      },
+    ],
   },
-];
-
-export const otherSoft = [
   {
     name: "Music",
-    description: "Spotify (YT Music is good too)",
-    url: "https://open.spotify.com/playlist/4oKjrCt8bfJCLQxL635Ftq",
+    softwares: [
+      {
+        name: "Spotify",
+        url: "https://open.spotify.com/playlist/4oKjrCt8bfJCLQxL635Ftq",
+      },
+      {
+        name: "YouTube Music",
+        url: "https://music.youtube.com",
+      },
+    ],
   },
   {
     name: "Password Manager",
-    description: "Bitwarden",
-    url: "https://bitwarden.com",
+    softwares: [
+      {
+        name: "Bitwarden",
+        url: "https://bitwarden.com",
+      },
+    ],
   },
   {
     name: "2FA",
-    description: "Twilio Authy",
-    url: "https://authy.com",
+    softwares: [
+      {
+        name: "Twilio Authy",
+        url: "https://authy.com",
+      },
+    ],
   },
   {
     name: "Main Browser",
-    description: "Mozilla Firefox",
-    url: "https://mozilla.org/en-US/firefox",
+    softwares: [
+      {
+        name: "Mozilla Firefox",
+        url: "https://mozilla.org/en-US/firefox",
+      },
+    ],
   },
   {
     name: "Secondary Browser",
-    description: "Brave",
-    url: "https://brave.com",
+    softwares: [
+      {
+        name: "Brave",
+        url: "https://brave.com",
+      },
+    ],
   },
   {
     name: "Email Client",
-    description: "MailSpring",
-    url: "https://getmailspring.com",
+    softwares: [
+      {
+        name: "MailSpring",
+        url: "https://getmailspring.com",
+      },
+    ],
   },
   {
     name: "Ad Blocker",
-    description: "uBlock Origin",
-    url: "https://ublockorigin.com",
+    softwares: [
+      {
+        name: "uBlock Origin",
+        url: "https://ublockorigin.com",
+      },
+    ],
   },
   {
     name: "Cloud Storage",
-    description: "Telegram",
-    url: "https://telegram.org",
+    softwares: [
+      {
+        name: "Telegram",
+        url: "https://telegram.org",
+      },
+    ],
   },
   {
     name: "Media Server",
-    description: "Jellyfin",
-    url: "https://jellyfin.org",
+    softwares: [
+      {
+        name: "Jellyfin",
+        url: "https://jellyfin.org",
+      },
+    ],
   },
-  // {
-  //   name: "Media Player",
-  //   description: "VLC",
-  //   url: "https://videolan.org/vlc/index.html",
-  // },
   {
-    name: "TV / Movies Tracker",
-    description: "Trakt.tv",
-    url: "https://trakt.tv",
+    name: "Movies & TV Shows Tracker",
+    softwares: [
+      {
+        name: "Trakt.tv",
+        url: "https://trakt.tv",
+      },
+    ],
   },
   {
     name: "Note Taking",
-    description: "Notion",
-    url: "https://notion.so",
+    softwares: [
+      {
+        name: "Notion",
+        url: "https://notion.so",
+      },
+    ],
   },
 ];
--- src/app/uses/page.js
+++ src/app/uses/page.js
@@ -1,93 +1,59 @@
 import { cn } from "@/lib/utils";
-import { hardware, devSoft, otherSoft } from "./data";
+import { hardware, softwaresList } from "./data";
 
 export const metadata = {
   title: "Uses",
 };
 
 const UsesPage = () => {
-  const softwares = devSoft.concat(otherSoft);
-
   return (
     <main className="container max-w-6xl pb-16">
       <section>
-        <p>A list of the hardware and software I use on a daily basis.</p>
-
-        <h2 className="text-3xl font-bold mb-2 mt-8">Hardware</h2>
-
-        <table className="table-auto w-full max-w-xl">
+        <table className="table-auto w-full">
+          <thead className="text-left">
+            <tr>
+              <th colSpan={2} className="text-2xl font-medium pb-2">
+                Hardware
+              </th>
+            </tr>
+          </thead>
           <tbody>
             {hardware.map((item, index) => (
               <tr key={item.name} className={cn(index !== hardware.length - 1 && "border-b")}>
                 <td className="font-medium py-2 align-top">{item.name}</td>
-                <td className="py-2 align-top pl-12">
-                  {item.description} {item.extra && <p className="text-sm opacity-80">{item.extra}</p>}
+                <td className="py-2 align-top pl-10 opacity-90 hover:opacity-100">
+                  {item.description}
+                  {item.extra && <p className="text-sm opacity-80">{item.extra}</p>}
                 </td>
               </tr>
             ))}
           </tbody>
-        </table>
 
-        <h2 className="text-3xl font-bold mb-2 mt-8">Software</h2>
-        <div className="flex items-start mb-2 gap-16 lg:hidden">
-          <table className="flex-1 w-full">
-            <tbody>
-              {devSoft.map((item, index) => (
-                <tr key={item.name} className={cn(index !== devSoft.length - 1 && "border-b")}>
-                  <td className="font-medium py-2 align-top">{item.name}</td>
-                  <td className="py-2 pl-12 align-top ">
-                    <a
-                      href={item.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="hover:underline underline-offset-4"
-                    >
-                      {item.description}
-                    </a>
-                    {item.extra && <p className="text-sm opacity-80">{item.extra}</p>}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-          <table className="flex-1 w-full">
-            <tbody>
-              {otherSoft.map((item, index) => (
-                <tr key={item.name} className={cn(index !== otherSoft.length - 1 && "border-b")}>
-                  <td className="font-medium py-2 align-top">{item.name}</td>
-                  <td className="py-2 pl-12 align-top">
-                    <a
-                      href={item.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="hover:underline underline-offset-4"
-                    >
-                      {item.description}
-                    </a>{" "}
-                    {item.extra && <p className="text-sm opacity-80">{item.extra}</p>}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-
-        <table className="hidden lg:table table-auto w-full">
+          <thead className="text-left">
+            <tr>
+              <th colSpan={2} className="text-2xl font-medium pt-8 pb-2">
+                Softwares, Services and Tools
+              </th>
+            </tr>
+          </thead>
           <tbody>
-            {softwares.map((item, index) => (
-              <tr key={item.name} className={cn(index !== softwares.length - 1 && "border-b")}>
+            {softwaresList.map((item, itemIndex) => (
+              <tr key={item.name} className={cn(itemIndex !== softwaresList.length - 1 && "border-b")}>
                 <td className="font-medium py-2 align-top">{item.name}</td>
-                <td className="py-2 pl-12 align-top">
-                  <a
-                    href={item.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="hover:underline underline-offset-4"
-                  >
-                    {item.description}
-                  </a>{" "}
-                  {item.extra && <p className="text-sm opacity-80">{item.extra}</p>}
+                <td className="py-2 align-top pl-10">
+                  {item.softwares.map((software, softwareIndex) => (
+                    <span key={software.name}>
+                      <a
+                        href={software.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="opacity-90 hover:opacity-100 hover:underline underline-offset-4"
+                      >
+                        {software.name}
+                      </a>
+                      {softwareIndex !== item.softwares.length - 1 && ", "}
+                    </span>
+                  ))}
                 </td>
               </tr>
             ))}
